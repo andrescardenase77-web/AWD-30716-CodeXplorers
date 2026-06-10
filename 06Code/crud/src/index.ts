58d8d005531cae24d8d344fdb342dda69427ad72
victoriadiazcaizaluisa <cvdiaz3@espe.edu.ec>
2026-06-10 00:42:24 -0500

    Patients CRUD URIs using prisma and typescript v1.0

--- 06Code/crud/src/index.ts
+++ 06Code/crud/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import supplyRouter from './routes/supplyRoutes';
+import patientRouter from './routes/patientRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +13,9 @@ app.set('json replacer', (key: string, value: any) => {
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/fabuladental', supplyRouter);
+app.use('/fabuladental', patientRouter);
 
 app.listen(port, () => {
   console.log(`Fábula Dental CRUD Server running on port ${port}`);
