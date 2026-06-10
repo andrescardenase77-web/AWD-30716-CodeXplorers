d0f5db9eb87b5e526f3d359a9c9a876e05be8adc
victoriadiazcaizaluisa <cvdiaz3@espe.edu.ec>
2026-06-10 01:21:33 -0500

    route dependency solution

--- 06Code/crud/src/index.ts
+++ 06Code/crud/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import supplyRouter from './routes/supplyRoutes';
+import patientRouter from './routes/patientRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/fabuladental', supplyRouter);
+app.use('/fabuladental', patientRouter);
 
 app.listen(port, () => {
   console.log(`Fábula Dental CRUD Server running on port ${port}`);
