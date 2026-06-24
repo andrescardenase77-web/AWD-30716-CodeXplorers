7efd7c446bad5dd8a2fcf3cef730fbb9e03f4009
Andres Cardenas <andrescardenase77@gmail.com>
2026-06-23 20:31:15 -0500

    Users module v 1.0 : Implement users database model, CRUD routes, and server x-api-key security

--- 06Code/crud/src/index.ts
+++ 06Code/crud/src/index.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import supplyRouter from './routes/supplyRoutes';
 import patientRouter from './routes/patientRoutes';
 import paymentRouter from './routes/paymentRoutes';
+import userRouter from './routes/userRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,9 +16,18 @@ app.set('json replacer', (key: string, value: any) => {
 app.use(cors());
 app.use(express.json());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const apiKey = req.headers['x-api-key'];
+  if (!apiKey || apiKey !== process.env.CRUD_API_KEY) {
+    return res.status(401).json({ error: "Authentication failure" });
+  }
+  return next();
+});
+
 app.use('/fabuladental', supplyRouter);
 app.use('/fabuladental', patientRouter);
 app.use('/fabuladental', paymentRouter);
+app.use('/fabuladental', userRouter);
 
 app.listen(port, () => {
   console.log(`Fábula Dental CRUD Server running on port ${port}`);
